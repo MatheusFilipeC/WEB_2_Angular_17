f024732c2c20d2a00fb0eaad168938c9d7347149
LAPTOP-UOMTMQDU\Murilo Santos <Murilo dos Santos>
2024-03-29 02:00:02 -0300

    [Web2][Consulta e Listagem]

--- lavanderia_lol/src/app/pages/consulta-pedido-cliente/consulta-pedido-cliente.component.ts
+++ lavanderia_lol/src/app/pages/consulta-pedido-cliente/consulta-pedido-cliente.component.ts
@@ -3,6 +3,7 @@ import { CommonModule } from '@angular/common';
 import { FormsModule } from '@angular/forms';
 import { RouterModule } from '@angular/router';
 import { Pedido, RoupasPedido, SharedModule } from '../../shared';
+import { PedidoService } from '../../services';
 
 @Component({
   selector: 'app-consulta-pedido-cliente',
@@ -17,6 +18,10 @@ import { Pedido, RoupasPedido, SharedModule } from '../../shared';
   styleUrl: './consulta-pedido-cliente.component.css'
 })
 export class ConsultaPedidoClienteComponent {
+  pedidos: Pedido[] = [];
+  constructor (private pedidoService: PedidoService) { 
+    this.pedidos = pedidoService.listarTodos();
+  }
 
   roupas: RoupasPedido[] = [];
 
@@ -28,26 +33,7 @@ export class ConsultaPedidoClienteComponent {
     {idPedido: 2, idRoupa: 34, nomeRoupa: 'Cueca', valorLavagemPeca: 12.00}
   ];
     
-    pedidos: Pedido[] = [
-      { idPedido: 1, dataPedido: new Date('2023-09-15T14:30:00'), dataEstimativa: new Date('2023-09-15T14:30:00'), dataColeta: new Date('2023-09-15T14:30:00'), 
-        dataEntrega: new Date('2023-09-15T14:30:00'), valor: 30.00, statusPedido: 'Em Aberto' },
-      { idPedido: 2, dataPedido: new Date('2023-09-20T10:00:00'), dataEstimativa: new Date('2023-09-20T10:00:00'), dataColeta: new Date('2023-09-20T10:00:00'), 
-        dataEntrega: new Date('2023-09-20T10:00:00'), valor: 49.90, statusPedido: 'Cancelado' },
-      { idPedido: 3, dataPedido: new Date('2023-10-05T08:45:00'), dataEstimativa: new Date('2023-10-05T08:45:00'), dataColeta: new Date('2023-10-05T08:45:00'), 
-        dataEntrega: new Date('2023-10-05T08:45:00'), valor: 40.50, statusPedido: 'Recolhido' },
-      { idPedido: 4, dataPedido: new Date('2023-10-10T13:15:00'), dataEstimativa: new Date('2023-10-10T13:15:00'), dataColeta: new Date('2023-10-10T13:15:00'), 
-        dataEntrega: new Date('2023-10-10T13:15:00'), valor: 110.50, statusPedido: 'Aguardando Pagamento' },
-      { idPedido: 5, dataPedido: new Date('2023-11-01T09:30:00'), dataEstimativa: new Date('2023-11-01T09:30:00'), dataColeta: new Date('2023-11-01T09:30:00'), 
-        dataEntrega: new Date('2023-11-01T09:30:00'), valor: 60.89, statusPedido: 'Pago' },
-      { idPedido: 6, dataPedido: new Date('2023-11-15T16:45:00'), dataEstimativa: new Date('2023-11-15T16:45:00'), dataColeta: new Date('2023-11-15T16:45:00'), 
-        dataEntrega: new Date('2023-11-15T16:45:00'), valor: 10.80, statusPedido: 'Finalizado' },
-      { idPedido: 7, dataPedido: new Date('2023-08-15T14:30:00'), dataEstimativa: new Date('2023-08-15T14:30:00'), dataColeta: new Date('2023-08-15T14:30:00'), 
-        dataEntrega: new Date('2023-08-15T14:30:00'), valor: 60.00, statusPedido: 'Em Aberto' },
-      { idPedido: 8, dataPedido: new Date('2023-09-22T14:30:00'), dataEstimativa: new Date('2023-09-22T14:30:00'), dataColeta: new Date('2023-09-22T14:30:00'), 
-        dataEntrega: new Date('2023-09-22T14:30:00'), valor: 80.00, statusPedido: 'Em Aberto' },
-      { idPedido: 9, dataPedido: new Date('2023-11-25T14:30:00'), dataEstimativa: new Date('2023-11-25T14:30:00'), dataColeta: new Date('2023-11-25T14:30:00'), 
-        dataEntrega: new Date('2023-11-25T14:30:00'), valor: 20.00, statusPedido: 'Em Aberto' },
-    ];
+
     
     roupa: RoupasPedido | undefined;
     pedido: number | null = null;
@@ -65,10 +51,4 @@ export class ConsultaPedidoClienteComponent {
         this.roupas = [];
       }
     }
-    
-    listarPedidos() {
-      return this.roupas.filter(r => r.idPedido === this.pedido);
-    }
-
-
 }
