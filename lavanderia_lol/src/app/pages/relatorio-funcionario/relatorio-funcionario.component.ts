10fae83163c60273060d4e9bc70b4012f507fc08
Matheus Filipe <68246210+MatheusFilipeC@users.noreply.github.com>
2024-03-28 22:58:59 -0300

    Adição geração de codigo para geração do pdf e importação do jspdf

--- lavanderia_lol/src/app/pages/relatorio-funcionario/relatorio-funcionario.component.ts
+++ lavanderia_lol/src/app/pages/relatorio-funcionario/relatorio-funcionario.component.ts
@@ -1,12 +1,30 @@
 import { Component } from '@angular/core';
+import jsPDF from 'jspdf';
 
 @Component({
   selector: 'app-relatorio-funcionario',
-  standalone: true,
-  imports: [],
   templateUrl: './relatorio-funcionario.component.html',
-  styleUrl: './relatorio-funcionario.component.css'
+  styleUrls: ['./relatorio-funcionario.component.css']
 })
 export class RelatorioFuncionarioComponent {
 
+  constructor() { }
+
+  gerarRelatorioReceitasPDF() {
+    const doc = new jsPDF();
+    doc.text('Relatório de Receitas', 10, 10);
+    doc.save('relatorio_receitas.pdf');
+  }
+
+  gerarRelatorioClientesPDF() {
+    const doc = new jsPDF();
+    doc.text('Relatório de Clientes', 10, 10);
+    doc.save('relatorio_clientes.pdf');
+  }
+
+  gerarRelatorioClientesFieisPDF() {
+    const doc = new jsPDF();
+    doc.text('Relatório de Clientes Fiéis', 10, 10);
+    doc.save('relatorio_clientes_fieis.pdf');
+  }
 }
